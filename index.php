9a0ca1425d373f8b287298a8d0b1886d681036fe
Diana Sari Agustiningsih <dianasari.20012@mhs.unesa.ac.id>
2022-10-24 16:25:52 +0700

    initial commit

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+    
+    echo "Hello World!<br>";
+
+    $nama = "Diana Sari Agustiningsih";
+    $umur = 21;
+
+    echo "Nama saya <strong>$nama</strong>, saya berusia $umur tahun.<br>";
+
+    $namaAdik = "Ayu Maulidia";
+    $umurAdik = 15;
+
+    echo "Nama adik saya <strong>$namaAdik</strong>, adik saya berusia $umueAdik tahun.<br>";
+
+    $selisih = $umur - $umurAdik;
+    echo "Selisih usia saya dan adik saya adalah $elisih tahun."
+
+?>
